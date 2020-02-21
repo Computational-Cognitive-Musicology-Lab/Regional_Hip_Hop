30fde49267a94040e9271eda8af2b46ad4cf71bd
Chalece DeLaCoudray <dahliablack22@gmail.com>
2020-02-20 20:58:49 -0500

    Lyric Text Files, Rhyme Analyzer Data
    Output Data from the Rhyme Analyzer tool, Lyrics in txt files, RHistory, load corpus data function

--- LoadData.R
+++ LoadData.R
@@ -1,13 +1,18 @@
-setwd("~/Bridge/Research/Projects/HipHop/RegionalFlow")
+setwd("/Users/ChaDe/Desktop/Regional_Hip_Hop")
 
 library(humdrumR)
 
-corpus <- readHumdrum('Regional_Hip_Hop/Rhythmic Transcriptions/^[1-5]/*.txt')
+corpus <- readHumdrum('Rhythmic Transcriptions/^[1-5]/*.txt')
 
 as.data.frame(as.matrix(corpus), stringsAsFactors = FALSE) -> data
 colnames(data) <- c('NoteValue', 'Stress', 'Rest', 'Lyrics')
 
 data$Rhythm <- as.decimal(data$NoteValue)
+
+corpus$ARE <- corpus %hum>% ~gsub(x = ARE, '\t*', '')
+corpus$OTL <- corpus %hum>% ~gsub(x = OTL, '\t*', '')
+corpus$COM <- corpus %hum>% ~gsub(x = COM, '\t*', '')
+corpus$RRD <- corpus %hum>% ~gsub(x = RRD, '\t*', '')
 
 data$OTL <- as.matrix(corpus, fieldnames = 'OTL')[,1]
 data$COM <- as.matrix(corpus, fieldnames = 'COM')[,1]
---
+++ hip_hop_analysis.R
@@ -0,0 +1,5 @@
+subset(data , ARE == 'Atlanta')
+read.table('Regional Hip Hop_Rhyme_Analysis_Data.csv', sep = ',', header=T)
+
+rhymeData <- Regional_Hip_Hop_Rhyme_Analysis_Data
+
